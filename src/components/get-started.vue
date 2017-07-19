92ab5c1b7f1a9035d35deff2b4fea8d1c3efeb4e
Hejx <531601727@qq.com>
2017-07-19 16:15:16 +0800

    Update get-started page.

--- src/components/get-started.vue
+++ src/components/get-started.vue
@@ -1,8 +1,22 @@
 <template>
   <v-page>
     <v-content>
+      <v-block-title># 安装</v-block-title>
       <v-content-block>
-        get started
+        <span>使用 npm 或者 yarn</span>
+        <ul class="code-preview">
+          <li>npm install vuego --save</li>
+        </ul>
+      </v-content-block>
+      <v-block-title># 使用</v-block-title>
+      <v-content-block>
+        <ul class="code-preview">
+          <li>import Vue from 'vue';</li>
+          <li>import Vuego from 'vuego';</li>
+          <li>import 'vuego/dist/vuego.css'; // import css file</li>
+          <li></li>
+          <li>Vue.use(Vuego);</li>
+        </ul>
       </v-content-block>
     </v-content>
   </v-page>
@@ -17,6 +31,16 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
+.code-preview {
+  background: #fff;
+  border: 1px dotted #eee;
+  padding: 10px;
+}
 
+.code-preview li {
+  list-style: none;
+  line-height: 24px;
+  min-height: 24px;
+}
 </style>
